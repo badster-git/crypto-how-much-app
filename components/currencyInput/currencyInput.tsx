534aec2cb935750bb2fb7419fd8de1f12d54ec83
badster-git <badster-main@protonmail.ch>
2022-10-02 00:30:58 -0700

    feat: updated site design and fixed minor bug in calculation

--- components/currencyInput/currencyInput.tsx
+++ components/currencyInput/currencyInput.tsx
@@ -11,10 +11,12 @@ interface ICryptoOptions {
 }
 interface IPriceInfo extends ICryptoOptions {
   locale: string;
-  profit: number;
   previousPrice: number;
   currentPrice: number;
-  totalMade: number;
+  dateBought: string;
+  amountBought: string;
+  profit: string;
+  totalMade: string;
 }
 interface ICurrencyOptions {
   abbreviation: string;
@@ -72,14 +74,22 @@ const currencyInputOptions: ICurrencyOptions[] = [
 ];
 
 const validationSchema = Yup.object().shape({
-  currency: Yup.number().required().min(1000).positive(),
+  currency: Yup.number()
+    .required("*required")
+    .min(1000, "*required")
+    .positive(),
   currencyType: Yup.string()
-    .required()
-    .oneOf(currencyInputOptions.map((val) => val.abbreviation)),
+    .required("*required")
+    .oneOf(
+      currencyInputOptions.map((val) => val.abbreviation),
+      "*required"
+    ),
   cryptoType: Yup.string()
-    .required()
+    .required("*required")
     .oneOf(cryptoInputOptions.map((val) => val.id)),
-  selectedDate: Yup.date().required().max(new Date().toLocaleDateString()),
+  selectedDate: Yup.date()
+    .required("*required")
+    .max(new Date().toLocaleDateString(), "date must be earlier"),
 });
 
 export function CurrencyInputForm() {
@@ -93,6 +103,7 @@ export function CurrencyInputForm() {
     },
     validationSchema: validationSchema,
     async onSubmit(values, actions) {
+      actions.setSubmitting(true);
       // grap data & fix up date for API
       const { currency, currencyType, cryptoType, selectedDate } = values;
       const [year, month, day] = selectedDate.split("-");
@@ -145,7 +156,7 @@ export function CurrencyInputForm() {
         previousCryptoHeld = Number(currency) / previousCryptoPrice;
 
       // calculate profit
-      const cryptoProfit = currentCryptoHeld - previousCryptoHeld,
+      const cryptoProfit = previousCryptoHeld - currentCryptoHeld,
         totalCryptoMade = cryptoProfit + currentCryptoHeld,
         currencyProfit = currentCryptoPrice * cryptoProfit,
         totalCurrencyMade = currentCryptoPrice * totalCryptoMade;
@@ -173,106 +184,183 @@ export function CurrencyInputForm() {
         locale: currencyType,
         previousPrice: previousCryptoPrice,
         currentPrice: currentCryptoPrice,
-        profit: currencyProfit,
-        totalMade: totalCurrencyMade,
+        dateBought: selectedDate,
+        amountBought: currency,
+        profit: CurrencyFormatter(currencyType, currencyProfit),
+        totalMade: CurrencyFormatter(currencyType, totalCurrencyMade),
       };
       // update state
       setPriceData(priceData);
+      actions.setSubmitting(false);
     },
   });
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <div className="flex items-center">
-          <CurrencyInput
-            id="nominal"
-            name="nominal"
-            placeholder={`Enter ${formik.values.currencyType.toUpperCase()}`}
-            defaultValue={0}
-            decimalsLimit={2}
-            allowNegativeValue={false}
-            step={500}
-            value={formik.values.currency}
-            onValueChange={(value) => {
-              console.log(value);
-              formik.setFieldValue("currency", value);
-            }}
-            prefix={`${currencyInputOptions.reduce((prev, val) => {
-              if (val.abbreviation == formik.values.currencyType) {
-                return val.symbol;
-              }
-              return prev;
-            }, "")} `}
-            className="transition duration-300 ease-in-out px-3 py-2 rounded border w-26 focus:outline-none focus:ring-2 focus:ring-blue-800"
-          />
-          <select
-            id="selectedCurrencyType"
-            name="currencyType"
-            value={formik.values.currencyType}
-            onChange={(value) => {
-              console.log(value);
-              formik.setFieldValue("currencyType", value.target.value);
-            }}
-          >
-            {currencyInputOptions.map((val, idx) => (
-              <option key={idx} value={val.abbreviation}>
-                {val.abbreviation}
-              </option>
-            ))}
-          </select>
-          <input
-            id="selectedDate"
-            type="date"
-            name="date"
-            value={formik.values.selectedDate}
-            onChange={(value) => {
-              formik.setFieldValue("selectedDate", value.target.value);
-            }}
-            className="transition duration-300 ease-in-out px-3 py-2 rounded border w-26 focus:outline-none focus:ring-2 focus:ring-blue-800"
-          />
-          <select
-            id="selectedCryptoType"
-            name="cryptoType"
-            value={formik.values.cryptoType}
-            onChange={(value) => {
-              console.log(value);
-              formik.setFieldValue("cryptoType", value.target.value);
-            }}
-          >
-            {cryptoInputOptions.map((val, idx) => (
-              <option key={idx} value={val.id}>
-                {val.symbol}
-              </option>
-            ))}
-          </select>
-          <button type="submit">Check</button>
-          {formik.errors.currency && formik.touched.currency && (
-            <div className={"text-red-700 pb-2"}>{formik.errors.currency}</div>
-          )}
-          {formik.errors.currencyType && formik.touched.currencyType && (
-            <div className={"text-red-700 pb-2"}>{formik.errors.currency}</div>
-          )}
-          {formik.errors.selectedDate && formik.touched.selectedDate && (
-            <div className={"text-red-700 pb-2"}>
-              {formik.errors.selectedDate}
+        <div className="md:flex mb-2">
+          <div className="md:w-1/3">
+            <legend className="uppercase tracking-wide text-sm">
+              What is this?
+            </legend>
+            <p className="text-xs font-light md:w-[15rem] mt-2">
+              Checks potential profits you could've made if you'd had invested
+              in a coin at a certain date.
+            </p>
+          </div>
+          <div className="md:flex w-full justify-center mt-2 mb:mt-0 md:px-3">
+            <div className="mt-4">
+              <select
+                id="selectedCurrencyType"
+                name="currencyType"
+                value={formik.values.currencyType}
+                onChange={(value) => {
+                  console.log(value);
+                  formik.setFieldValue("currencyType", value.target.value);
+                }}
+                className="transition duration-300 ease-in-out px-3 py-2 rounded border w-26 focus:outline-none focus:ring-2 focus:ring-blue-800"
+              >
+                {currencyInputOptions.map((val, idx) => (
+                  <option key={idx} value={val.abbreviation}>
+                    {val.abbreviation}
+                  </option>
+                ))}
+              </select>
+              {formik.errors.currency && formik.touched.currency && (
+                <div
+                  className={
+                    "text-red-700 pb-2 ml-3 absolute w-[8rem] font-light text-sm"
+                  }
+                >
+                  {formik.errors.currencyType}
+                </div>
+              )}
             </div>
-          )}
-          {formik.errors.cryptoType && formik.touched.cryptoType && (
-            <div className={"text-red-700 pb-2"}>
-              {formik.errors.cryptoType}
+            <div className="mb-4 ml-2">
+              <label className="block uppercase tracking-wide text-xs font-bold">
+                Amount
+              </label>
+              <CurrencyInput
+                id="nominal"
+                name="nominal"
+                placeholder={`Enter ${formik.values.currencyType.toUpperCase()}`}
+                defaultValue={0}
+                decimalsLimit={2}
+                allowNegativeValue={false}
+                step={500}
+                value={formik.values.currency}
+                onValueChange={(value) => {
+                  console.log(value);
+                  formik.setFieldValue("currency", value);
+                }}
+                prefix={`${currencyInputOptions.reduce((prev, val) => {
+                  if (val.abbreviation == formik.values.currencyType) {
+                    return val.symbol;
+                  }
+                  return prev;
+                }, "")} `}
+                className="transition duration-300 ease-in-out px-3 py-2 rounded border w-26 focus:outline-none focus:ring-2 focus:ring-blue-800"
+              />
+              {formik.errors.currency && formik.touched.currency && (
+                <div
+                  className={
+                    "text-red-700 pb-2 ml-3 absolute w-[8rem] font-light text-sm"
+                  }
+                >
+                  {formik.errors.currency}
+                </div>
+              )}
             </div>
-          )}
+            <div className="mb-4 ml-2">
+              <label className="block uppercase tracking-wide text-xs font-bold">
+                Date
+              </label>
+              <input
+                id="selectedDate"
+                type="date"
+                name="date"
+                value={formik.values.selectedDate}
+                onChange={(value) => {
+                  formik.setFieldValue("selectedDate", value.target.value);
+                }}
+                className="transition duration-300 ease-in-out px-3 py-2 rounded border w-26 focus:outline-none focus:ring-2 focus:ring-blue-800"
+              />
+              {formik.errors.selectedDate && formik.touched.selectedDate && (
+                <div
+                  className={
+                    "text-red-700 pb-2 ml-3 absolute w-[8rem] font-light text-sm"
+                  }
+                >
+                  {formik.errors.selectedDate}
+                </div>
+              )}
+            </div>
+            <div className="ml-2 mt-4">
+              <select
+                id="selectedCryptoType"
+                name="cryptoType"
+                value={formik.values.cryptoType}
+                onChange={(value) => {
+                  console.log(value);
+                  formik.setFieldValue("cryptoType", value.target.value);
+                }}
+                className="transition duration-300 ease-in-out px-3 py-2 rounded border w-26 focus:outline-none focus:ring-2 focus:ring-blue-800"
+              >
+                {cryptoInputOptions.map((val, idx) => (
+                  <option key={idx} value={val.id}>
+                    {val.symbol}
+                  </option>
+                ))}
+              </select>
+              {formik.errors.cryptoType && formik.touched.cryptoType && (
+                <div
+                  className={
+                    "text-red-700 pb-2 ml-3 absolute w-[8rem] font-light text-sm"
+                  }
+                >
+                  {formik.errors.cryptoType}
+                </div>
+              )}
+            </div>
+            <div className="ml-2 mt-4">
+              <button
+                type="submit"
+                className="transition duration-300 ease-in-out px-3 py-2 text-cream-light rounded border w-26  bg-brick hover:bg-brick-dark"
+                disabled={formik.isSubmitting}
+              >
+                Check Profits
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="md:flex mb-8">
+          <div className="md:w-1/3">
+            <legend className="uppercase tracking-wide text-sm">Why?</legend>
+            <p className="text-xs font-light md:w-[15rem] mt-2">
+              Just felt like making it. No real reason.
+            </p>
+          </div>
+          <div className="md:flex mb-4 justify-center">
+            {priceData && (
+              <div className="text-center mt-2">
+                <p className="text-md font-light">
+                  If you had bought{" "}
+                  <span id="amountBought">
+                    {priceData?.symbol}
+                    {priceData?.amountBought}{" "}
+                  </span>
+                  of {priceData.id} on {priceData.dateBought} you would have
+                  around <span id="totalMade">{priceData?.totalMade}</span> right now.
+                </p>
+
+                <p className="text-md font-light">
+                  A total of about {priceData.profit} in profit.
+                </p>
+              </div>
+            )}
+          </div>
         </div>
       </form>
-      {priceData && (
-        <div>
-          {priceData.id} {priceData.locale}{" "}
-          {CurrencyFormatter(priceData.locale, priceData.profit)}{" "}
-          {priceData.symbol}{" "}
-          {CurrencyFormatter(priceData.locale, priceData.totalMade)}
-        </div>
-      )}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cream': {
+          light: '#F9F9FA',
+          DEFAULT: '#F4F4F0',
+          dark: '#FFE9C9'
+        },
+        'brick': {
+          DEFAULT: '#93625b',
+          dark: '#75413F'
+        }
+      }
+    },
   },
   plugins: [],
 }
